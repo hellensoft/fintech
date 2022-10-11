364eeaa3e7939b40ae01a58e8c65daacc13efe3f
IbrahimOwner2020 <kidibra2020@gmail.com>
2022-10-11 15:04:03 +0300

    refactor: code cleanup and setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IApp {}
+
+const App: FC<IApp> = () => {
+    return (
+        <div className='bg-black'>
+            <h2 className='text-4xl text-red-600'>The app has started</h2>
+        </div>
+    )
 }
 
-export default App;
+export default App
