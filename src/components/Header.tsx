08023ce3aa63cc20e13e3848054a8a0eb185afbf
IbrahimOwner2020 <kidibra2020@gmail.com>
2023-01-04 15:22:47 +0300

    fix: popover

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,181 +10,182 @@ import { openPhoneMenu } from "../recoil/atom";
 import { IoClose } from "react-icons/io5";
 
 interface IHeader {
-	fixed?: boolean;
+    fixed?: boolean;
 }
 
 const Header: FC<IHeader> = ({ fixed }) => {
-	const [scrolled, setScrolled] = useState(false);
-	const { pathname } = useLocation();
-	const [openSideBar, setOpenSideBar] = useRecoilState(openPhoneMenu);
+    const [scrolled, setScrolled] = useState(false);
+    const { pathname } = useLocation();
+    const [openSideBar, setOpenSideBar] = useRecoilState(openPhoneMenu);
 
-	useEffect(() => {
-		const handler = () => {
-			setScrolled((scrolled) => {
-				if (
-					!scrolled &&
-					(document.body.scrollTop > 20 ||
-						document.documentElement.scrollTop > 20)
-				) {
-					return true;
-				}
+    useEffect(() => {
+        const handler = () => {
+            setScrolled((scrolled) => {
+                if (
+                    !scrolled &&
+                    (document.body.scrollTop > 20 ||
+                        document.documentElement.scrollTop > 20)
+                ) {
+                    return true;
+                }
 
-				if (
-					scrolled &&
-					document.body.scrollTop < 4 &&
-					document.documentElement.scrollTop < 4
-				) {
-					return false;
-				}
+                if (
+                    scrolled &&
+                    document.body.scrollTop < 4 &&
+                    document.documentElement.scrollTop < 4
+                ) {
+                    return false;
+                }
 
-				return scrolled;
-			});
-		};
+                return scrolled;
+            });
+        };
 
-		window.addEventListener("scroll", handler);
-		return () => window.removeEventListener("scroll", handler);
-	});
+        window.addEventListener("scroll", handler);
+        return () => window.removeEventListener("scroll", handler);
+    });
 
-	return (
-		<div
-			className={`sticky top-0 border-b bg-[#fffe] backdrop-blur z-30 ${
-				scrolled && "shadow-lg"
-			}`}
-		>
-			<SectionWrapper>
-				<div
-					className={`flex items-center justify-between ${
-						scrolled || fixed ? "py-2 2lg:py-4" : "py-2 sm:py-6 2lg:py-8"
-					} duration-200`}
-				>
-					<div>
-						<Link to="/">
-							<img
-								className="h-12 md:h-16 w-auto"
-								src="/images/logo-words.png"
-								alt="logo"
-							/>
-						</Link>
-					</div>
-					<button
-						onClick={() => setOpenSideBar(!openSideBar)}
-						className="ml-auto space-x-8 inline-block 2lg:hidden z-40"
-					>
-						{openSideBar ? (
-							<IoClose className="text-blackPrimary h-7 w-7" />
-						) : (
-							<CgMenu className="text-blackPrimary h-7 w-7" />
-						)}
-					</button>
-					<div className="ml-auto space-x-5 2xl:space-x-8 hidden 2lg:flex items-center">
-						{navigations.map((nav, index) => (
-							<Fragment key={index}>
-								{nav.multiple ? (
-									<Popover key={index} className="relative">
-										{({ open }) => (
-											<>
-												<Popover.Button
-													className={`hover:text-greenPrimary font-medium focus:outline-none flex items-center ${
-														open ||
-														nav.links?.some(
-															(el) =>
-																el.link ===
-																pathname
-														)
-															? "text-greenPrimary"
-															: "text-bluePrimary"
-													} duration-300`}
-												>
-													<span>{nav.name}</span>
-													<BsChevronDown
-														className={`ml-1 duration-300 ${
-															open
-																? "rotate-180"
-																: "rotate-0"
-														}`}
-													/>
-												</Popover.Button>
-												<Transition
-													as={Fragment}
-													enter="transition duration-100 ease-out"
-													enterFrom="transform scale-95 opacity-0"
-													enterTo="transform scale-100 opacity-100"
-													leave="transition duration-75 ease-out"
-													leaveFrom="transform scale-100 opacity-100"
-													leaveTo="transform scale-95 opacity-0"
-												>
-													<Popover.Panel className="absolute z-10">
-														<div className="bg-white shadow-md rounded mt-4 w-auto min-w-[250px] border flex flex-col items-start divide-y">
-															{nav.links?.map(
-																(
-																	link,
-																	index
-																) => (
-																	<Popover.Button
-																		as={
-																			Fragment
-																		}
-																		key={
-																			index
-																		}
-																	>
-																		<NavLink
-																			end={
-																				link.end
-																			}
-																			to={
-																				link.link ||
-																				"/"
-																			}
-																			className={({
-																				isActive,
-																			}) =>
-																				`hover:text-greenPrimary font-medium text-bluePrimary hover:bg-[#eee] px-4 py-2 w-full ${
-																					isActive &&
-																					"bg-[#eee] text-greenPrimary"
-																				}`
-																			}
-																		>
-																			{
-																				link.name
-																			}
-																		</NavLink>
-																	</Popover.Button>
-																)
-															)}
-														</div>
-													</Popover.Panel>
-												</Transition>
-											</>
-										)}
-									</Popover>
-								) : (
-									<NavLink
-										key={index}
-										to={nav.link || "/"}
-										end={nav.end}
-										className={({ isActive }) =>
-											`hover:text-greenPrimary font-medium text-bluePrimary ${
-												isActive && "text-greenPrimary"
-											}`
-										}
-									>
-										{nav.name}
-									</NavLink>
-								)}
-							</Fragment>
-						))}
-						<Link
-							className="font-medium bg-bluePrimary hover:bg-greenPrimary duration-300 py-2 px-6 rounded-md text-white"
-							to="/enquiry"
-						>
-							Enquire Now
-						</Link>
-					</div>
-				</div>
-			</SectionWrapper>
-		</div>
-	);
+    return (
+        <div
+            className={`sticky top-0 border-b bg-[#fffe] backdrop-blur z-30 ${
+                scrolled && "shadow-lg"
+            }`}
+        >
+            <SectionWrapper>
+                <div
+                    className={`flex items-center justify-between ${
+                        scrolled || fixed
+                            ? "py-2 2lg:py-4"
+                            : "py-2 sm:py-6 2lg:py-8"
+                    } duration-200`}
+                >
+                    <div>
+                        <Link to="/">
+                            <img
+                                className="h-12 md:h-16 w-auto"
+                                src="/images/logo-words.png"
+                                alt="logo"
+                            />
+                        </Link>
+                    </div>
+                    <button
+                        onClick={() => setOpenSideBar(!openSideBar)}
+                        className="ml-auto space-x-8 inline-block 2lg:hidden z-40"
+                    >
+                        {openSideBar ? (
+                            <IoClose className="text-blackPrimary h-7 w-7" />
+                        ) : (
+                            <CgMenu className="text-blackPrimary h-7 w-7" />
+                        )}
+                    </button>
+                    <div className="ml-auto space-x-5 2xl:space-x-8 hidden 2lg:flex items-center">
+                        {navigations.map((nav, index) => (
+                            <Fragment key={index}>
+                                {nav.multiple ? (
+                                    <Popover key={index} className="relative">
+                                        {({ open }) => (
+                                            <>
+                                                <Popover.Button
+                                                    className={`hover:text-greenPrimary font-medium focus:outline-none flex items-center ${
+                                                        open ||
+                                                        nav.links?.some(
+                                                            (el) =>
+                                                                el.link ===
+                                                                pathname
+                                                        )
+                                                            ? "text-greenPrimary"
+                                                            : "text-bluePrimary"
+                                                    } duration-300`}
+                                                >
+                                                    <span>{nav.name}</span>
+                                                    <BsChevronDown
+                                                        className={`ml-1 duration-300 ${
+                                                            open
+                                                                ? "rotate-180"
+                                                                : "rotate-0"
+                                                        }`}
+                                                    />
+                                                </Popover.Button>
+                                                <Transition
+                                                    as={Fragment}
+                                                    enter="transition duration-100 ease-out"
+                                                    enterFrom="transform scale-95 opacity-0"
+                                                    enterTo="transform scale-100 opacity-100"
+                                                    leave="transition duration-75 ease-out"
+                                                    leaveFrom="transform scale-100 opacity-100"
+                                                    leaveTo="transform scale-95 opacity-0"
+                                                >
+                                                    <Popover.Panel className="absolute z-10">
+                                                        <div className="bg-white shadow-md rounded mt-4 w-auto min-w-[250px] border flex flex-col items-start divide-y">
+                                                            {nav.links?.map(
+                                                                (
+                                                                    link,
+                                                                    index
+                                                                ) => (
+                                                                    <Popover.Button
+                                                                        as={
+                                                                            NavLink
+                                                                        }
+                                                                        end={
+                                                                            link.end
+                                                                        }
+                                                                        to={
+                                                                            link.link ||
+                                                                            "/"
+                                                                        }
+                                                                        className={({
+                                                                            isActive,
+                                                                        }: {
+                                                                            isActive: boolean;
+                                                                        }) =>
+                                                                            `hover:text-greenPrimary font-medium text-bluePrimary hover:bg-[#eee] px-4 py-2 w-full ${
+                                                                                isActive &&
+                                                                                "bg-[#eee] text-greenPrimary"
+                                                                            }`
+                                                                        }
+                                                                        key={
+                                                                            index
+                                                                        }
+                                                                    >
+                                                                        {
+                                                                            link.name
+                                                                        }
+                                                                    </Popover.Button>
+                                                                )
+                                                            )}
+                                                        </div>
+                                                    </Popover.Panel>
+                                                </Transition>
+                                            </>
+                                        )}
+                                    </Popover>
+                                ) : (
+                                    <NavLink
+                                        key={index}
+                                        to={nav.link || "/"}
+                                        end={nav.end}
+                                        className={({ isActive }) =>
+                                            `hover:text-greenPrimary font-medium text-bluePrimary ${
+                                                isActive && "text-greenPrimary"
+                                            }`
+                                        }
+                                    >
+                                        {nav.name}
+                                    </NavLink>
+                                )}
+                            </Fragment>
+                        ))}
+                        <Link
+                            className="font-medium bg-bluePrimary hover:bg-greenPrimary duration-300 py-2 px-6 rounded-md text-white"
+                            to="/enquiry"
+                        >
+                            Enquire Now
+                        </Link>
+                    </div>
+                </div>
+            </SectionWrapper>
+        </div>
+    );
 };
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    screens: {
-      xs: "450px",
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      "2lg": "1100px",
-      xl: "1280px",
-      "2xl": "1536px",
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        screens: {
+            xs: "450px",
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            "2lg": "1100px",
+            xl: "1280px",
+            "2xl": "1536px",
+        },
+        extend: {
+            colors: {
+                blackPrimary: "#000008",
+                bluePrimary: "#29388d",
+                grayText: "#52576a",
+                grayText2: "#666666",
+                blueTransparent: "#29388dc9",
+                greenPrimary: "#09793d",
+                greenTrans: "#09793ddd",
+                yellowPrimary: "#ffb703",
+            },
+        },
     },
-    extend: {
-      colors: {
-        blackPrimary: "#000008",
-        bluePrimary: "#29388d",
-        grayText: "#52576a",
-        grayText2: "#666666",
-        blueTransparent: "#29388dc9",
-        greenPrimary: "#09793d",
-        greenTrans: "#09793ddd",
-        yellowPrimary: "#ffb703",
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/forms")],
+    plugins: [require("@tailwindcss/forms")],
 };
