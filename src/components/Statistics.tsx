d40bd5058eaf8afbf8581f73ba3413844402851c
Ibrahim John <kidibra2020@gmail.com>
2023-06-26 20:25:00 +0300

    update home page statistics

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -5,30 +5,29 @@ import statistics from "../data/statics.json";
 interface IStatistics {}
 
 const Statistics: FC<IStatistics> = () => {
-	return (
-		<div className="bg-bluePrimary">
-			<SectionWrapper>
-				<div className="py-24">
-					{/* <h2 className="text-white font-semibold text-center mb-20 text-3xl">The Heading to be given by Francis</h2> */}
-					<div className="grid grid-cols-2 sm:grid-cols-5 gap-x-4 gap-y-12 sm:gap-0 md:gap-12">
-						{statistics.map((data, index) => (
-							<div
-								className="flex flex-col items-center"
-								key={index}
-							>
-								<h2 className="text-white font-semibold text-3xl sm:text-4xl">
-									{data.number}
-								</h2>
-								<p className="text-[#fffe] font-medium text-base sm:text-xl mt-2">
-									{data.name}
-								</p>
-							</div>
-						))}
-					</div>
-				</div>
-			</SectionWrapper>
-		</div>
-	);
+    return (
+        <div className="bg-bluePrimary">
+            <SectionWrapper>
+                <div className="py-24">
+                    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-x-4 gap-y-12 lg:gap-0 md:gap-8">
+                        {statistics.map((data, index) => (
+                            <div
+                                className="flex flex-col items-center"
+                                key={index}
+                            >
+                                <h2 className="text-white font-semibold text-3xl sm:text-4xl">
+                                    {data.number}
+                                </h2>
+                                <p className="text-[#fffe] font-medium text-sm lg:text-base text-center mt-2">
+                                    {data.name}
+                                </p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </SectionWrapper>
+        </div>
+    );
 };
 
 export default Statistics;
