8976b01b5d6a4dcd4c970acab9ade0c313289b85
Ibrahim John <kidibra2020@gmail.com>
2023-08-03 12:24:36 +0300

    add agenda page

--- src/pages/Agenda.tsx
+++ src/pages/Agenda.tsx
@@ -1,104 +1,257 @@
 import { FC } from "react";
-import { Link } from "react-router-dom";
 import PageIntro from "../components/PageIntro";
 import SectionWrapper from "../components/SectionWrapper";
-import teamMembers from "../data/teamMember.json";
+import TakeTheStage from "../components/TakeTheState";
 
-interface IAboutUs {}
+interface IAgenda {}
 
-const AboutUs: FC<IAboutUs> = () => {
+const Agenda: FC<IAgenda> = () => {
+    const timetableData = [
+        {
+            conferenceTitle: "The Future of Banking Conference",
+            date: "08/11/2023",
+            timeSlots: [
+                {
+                    time: "08:00 am - 09:00 am",
+                    activity: "Breakfast and Registration",
+                },
+                {
+                    time: "09:00 am - 10:30 am",
+                    activity:
+                        'Presentation on "Digital Transformation in Banking"',
+                },
+                {
+                    time: "10:00 am - 11:00 am",
+                    activity: "Guest of Honour Session",
+                },
+                {
+                    time: "11:00 am - 11:15 am",
+                    activity: "Coffee Break",
+                },
+                {
+                    time: "11:15 am - 12:45 pm",
+                    activity:
+                        'Panel Discussion on "AI and Automation in Banking"',
+                },
+                {
+                    time: "12:45 pm - 02:00 pm",
+                    activity: "Lunch Break",
+                },
+                {
+                    time: "02:00 pm - 03:30 pm",
+                    activity:
+                        'Presentation on "Open Banking and Collaborative Partnerships"',
+                },
+                {
+                    time: "03:30 pm - 03:45 pm",
+                    activity: "Tea Break",
+                },
+                {
+                    time: "03:45 pm - 04:30 pm",
+                    activity:
+                        'Panel Discussion on "Cybersecurity and Data Privacy in Banking"',
+                },
+            ],
+        },
+        {
+            conferenceTitle: "The Future of E-commerce Africa Conference",
+            date: "08/11/2023",
+            timeSlots: [
+                {
+                    time: "08:00 am - 09:00 am",
+                    activity: "Breakfast and Registratio\n",
+                },
+                {
+                    time: "09:00 am - 10:30 am",
+                    activity:
+                        'Presentation on "E-commerce Market Trends in Africa"',
+                },
+                { time: "10:30 am - 10:45 am", activity: "Coffee Break" },
+                {
+                    time: "10:45 am - 12:15 pm",
+                    activity:
+                        'Panel Discussion on "Digital Payments and E-commerce"',
+                },
+                { time: "12:15 pm - 01:30 pm", activity: "Lunch Break" },
+                {
+                    time: "01:30 pm - 03:00 pm",
+                    activity:
+                        'Presentation on "Logistics and Last-Mile Delivery Challenges"',
+                },
+                { time: "03:00 pm - 03:15 pm", activity: "Tea Break" },
+                {
+                    time: "03:15 pm - 04:00 pm",
+                    activity: 'Panel Discussion on "Cross-Border E-commerce"',
+                },
+            ],
+        },
+        {
+            conferenceTitle: "The Future of Insurance Africa Conference",
+            date: "08/11/2023",
+            timeSlots: [
+                {
+                    time: "08:00 am - 09:00 am",
+                    activity: "Breakfast and Registration",
+                },
+                {
+                    time: "09:00 am - 10:30 am",
+                    activity: 'Presentation on "Insurtech Innovations"',
+                },
+                { time: "10:30 am - 10:45 am", activity: "Coffee Break" },
+                {
+                    time: "10:45 am - 12:15 pm",
+                    activity:
+                        'Panel Discussion on "Microinsurance and Inclusive Insurance"',
+                },
+                { time: "12:15 pm - 01:30 pm", activity: "Lunch Break" },
+                {
+                    time: "01:30 pm - 03:00 pm",
+                    activity:
+                        'Presentation on "Data Analytics and Risk Management"',
+                },
+                { time: "03:00 pm - 03:15 pm", activity: "Tea Break" },
+                {
+                    time: "03:15 pm - 04:00 pm",
+                    activity:
+                        'Panel Discussion on "Regulatory Challenges and Opportunities"',
+                },
+            ],
+        },
+        {
+            conferenceTitle: "The Future of Microfinance Conference",
+            date: "09/11/2023",
+            timeSlots: [
+                {
+                    time: "08:00 am - 09:00 am",
+                    activity: "Breakfast and Registration",
+                },
+                {
+                    time: "09:00 am - 10:30 am",
+                    activity:
+                        'Presentation on "Digital Transformation in Microfinance"',
+                },
+                { time: "10:30 am - 10:45 am", activity: "Coffee Break" },
+                {
+                    time: "10:45 am - 12:15 pm",
+                    activity:
+                        'Panel Discussion on "Fintech and Microfinance Integration"',
+                },
+                { time: "12:15 pm - 01:30 pm", activity: "Lunch Break" },
+                {
+                    time: "01:30 pm - 03:00 pm",
+                    activity:
+                        'Presentation on "Microfinance Innovations for Financial Inclusion"',
+                },
+                { time: "03:00 pm - 03:15 pm", activity: "Tea Break" },
+                {
+                    time: "03:15 pm - 04:00 pm",
+                    activity:
+                        'Panel Discussion on "Challenges and Opportunities in Microfinance"',
+                },
+                { time: "06:00 pm - 11:00 pm", activity: "Gala Dinner" },
+            ],
+        },
+        {
+            conferenceTitle: "The Future of Blockchain Conference Africa",
+            date: "09/11/2023",
+            timeSlots: [
+                {
+                    time: "08:00 am - 09:00 am",
+                    activity: "Breakfast and Registration",
+                },
+                {
+                    time: "09:00 am - 10:30 am",
+                    activity: 'Presentation on "Blockchain for Social Impact"',
+                },
+                { time: "10:30 am - 10:45 am", activity: "Coffee Break" },
+                {
+                    time: "10:45 am - 12:15 pm",
+                    activity:
+                        'Panel Discussion on "Decentralized Finance (DeFi)"',
+                },
+                { time: "12:15 pm - 01:30 pm", activity: "Lunch Break" },
+                {
+                    time: "01:30 pm - 03:00 pm",
+                    activity:
+                        'Presentation on "Blockchain Adoption in Supply Chain Management"',
+                },
+                { time: "03:00 pm - 03:15 pm", activity: "Tea Break" },
+                {
+                    time: "03:15 pm - 04:00 pm",
+                    activity:
+                        'Panel Discussion on "Regulatory Frameworks for Blockchain"',
+                },
+                { time: "06:00 pm - 11:00 pm", activity: "Gala Dinner" },
+            ],
+        },
+    ];
+
     return (
         <div>
             <PageIntro
-                title="About Fintech Tanzania"
-                description="Meet Fintech Team"
+                title="FinTech Festivals Tanzania Agenda"
+                description="Fintech Festivals Tanzania 2023 Schedule"
             />
             <SectionWrapper>
-                <div className="py-12 md:py-16">
-                    <div className="pt-12 md:pt-20 pb-4 md:pb-20 grid grid-cols-1 md:grid-cols-2 gap-12 content-center items-center">
-                        <div className="space-y-6">
-                            <h1 className="text-bluePrimary text-2xl sm:text-3xl text-center font-semibold">
-                                OUR HISTORY
-                            </h1>
-                            <p className="text-grayText text-sm sm:text-base font-medium">
-                                The 2019 edition of Tanzania Fintech and Banking
-                                Conference as part of Fintech Festivals Tanzania
-                                2023, previously known as Tanzania Financial
-                                Services expo, had attracted more than 100
-                                exhibiting companies from over 15 countries.
-                            </p>
-                            <p className="text-grayText text-sm sm:text-base font-medium">
-                                The exhibition also saw over 20 start-ups and
-                                hosted 4 country pavilions – France, India,
-                                Switzerland and USA. C-suite, senior management
-                                level decision makers and influencers made up
-                                40% of total conference attendees.
-                            </p>
-                            <p className="text-grayText text-sm sm:text-base font-medium">
-                                Fintech Festivals Tanzania 2023 is guaranteed to
-                                welcome over 3,000 international participants at
-                                the 2day festival with the vast majority being
-                                C-suite, Heads-of-Departments, key decision
-                                makers, key influencers and operators.
-                            </p>
-                        </div>
-                        <div>
-                            <img
-                                src="/images/photo_2023-01-28_11-27-26.jpeg"
-                                className="rounded-tl-[50px] rounded-br-[50px]"
-                                alt="fintech"
-                            />
-                        </div>
-                    </div>
-                </div>
-            </SectionWrapper>
-            <SectionWrapper>
-                <div className="py-10 md:py-16">
-                    <div className="max-w-[787px] mx-auto text-center mb-8">
-                        <h1 className="text-bluePrimary text-2xl sm:text-3xl font-semibold mb-4">
-                            Fintech Team Members
-                        </h1>
-                        <p className="text-grayText text-sm sm:text-base text-medium">
-                            Our team comprises senior leaders in Financial
-                            Services, Innovation and Media with solid expertise
-                            in delivering compelling and memorable top-quality
-                            events.
-                        </p>
-                    </div>
-                    <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-8 gap-8">
-                        {teamMembers.map((member, index) => (
-                            <div key={index} className="text-center group">
-                                <div className="w-full border-4 border-transparent aspect-square group-hover:border-4 group-hover:border-bluePrimary rounded-tl-[30px] rounded-br-[30px] duration-300 overflow-hidden">
-                                    <img
-                                        className="w-full group-hover:scale-110 duration-300 h-full object-cover object-center"
-                                        src={member.image}
-                                        alt={member.name}
-                                    />
+                <div className="flex flex-col max-w-4xl mx-auto px-4 py-20 space-y-4">
+                    <h1 className="text-xl sm:text-3xl text-bluePrimary font-semibold mb-4 text-center">
+                        Proposed schedule Fintech festival Tanzania 2023
+                    </h1>
+                    <div className="py-12 gap-y-8">
+                        {timetableData.map((conference, index) => (
+                            <div
+                                key={index}
+                                className="grid grid-cols-2 gap-8 relative"
+                            >
+                                <div className="py-12 relative">
+                                    <div className="sticky top-32">
+                                        <h2 className="text-xl font-bold">
+                                            {conference.conferenceTitle}
+                                        </h2>
+                                        <p className="text-gray-600">
+                                            Date: {conference.date}
+                                        </p>
+                                    </div>
                                 </div>
-                                <h3 className="text-blackPrimary text-lg sm:text-xl mt-3 font-semibold">
-                                    {member.name}
-                                </h3>
-                                <p className="text-bluePrimary uppercase font-semibold text-xs sm:text-sm">
-                                    {member.position}
-                                </p>
+                                <ol className="ml-8 my-8 py-2 max-w-3xl border-l border-gray-200 dark:border-gray-700">
+                                    {conference.timeSlots.map((time, index) => (
+                                        <li
+                                            className="my-8 ml-6 relative"
+                                            key={index}
+                                        >
+                                            <div className="absolute top-1.5 w-3 h-3 bg-gray-200 rounded-full -left-[30px] border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                                            <time className="mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                                                {time.time}
+                                            </time>
+                                            {/* <div className="">
+                                                <h3 className="text-md font-medium text-gray-500 dark:text-gray-400">
+                                                    Organization:{" "}
+                                                    {time.organization}
+                                                </h3>
+                                            </div> */}
+                                            <div className="flex items-end justify-between">
+                                                <h3 className="text-lg font-semibold text-purplePrimary dark:text-grayLighter">
+                                                    {time.activity}
+                                                </h3>
+                                                {/* <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                                                    {time.jobType} -{" "}
+                                                    {time.location}
+                                                </p> */}
+                                            </div>
+                                            {/* <p className="my-2 text-base font-normal text-gray-500 dark:text-gray-400">
+                                                {time.description}
+                                            </p> */}
+                                        </li>
+                                    ))}
+                                </ol>
                             </div>
                         ))}
                     </div>
                 </div>
-                <div className="mb-20 flex-col sm:flex-row space-y-6 sm:space-y-0 max-w-5xl flex items-center justify-between mx-auto border shadow-lg rounded-md py-8 px-8">
-                    <h2 className="text-xl text-center sm:text-start xs:text-3xl text-blackPrimary font-semibold">
-                        Become a member
-                    </h2>
-                    <Link
-                        className="font-medium bg-bluePrimary hover:bg-greenPrimary duration-300 py-2 px-6 rounded-md text-white"
-                        to="/contact"
-                    >
-                        Contact Us
-                    </Link>
-                </div>
             </SectionWrapper>
+            <TakeTheStage />
         </div>
     );
 };
 
-export default AboutUs;
+export default Agenda;
