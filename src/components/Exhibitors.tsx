0f29c92e9394b980362a07b0ecbd6e23e788242f
Ibrahim John <kidibra2020@gmail.com>
2023-08-04 18:26:14 +0300

    feat: add exhibitiors

--- src/components/Exhibitors.tsx
+++ src/components/Exhibitors.tsx
@@ -1,58 +1,102 @@
-import { FC, Key } from "react";
+import { FC, Key, useEffect, useState } from "react";
 import "swiper/css";
 import { Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SectionWrapper from "./SectionWrapper";
-import data from "../data/exhibitors.json";
+import { graphqlFetcher } from "../utils";
 
 interface IExhibitors {}
 
-const Exhibitors: FC<IExhibitors> = () => (
-	<SectionWrapper>
-		<div className="py-24">
-			<h1 className="text-2xl sm:text-3xl text-bluePrimary font-semibold mb-4 text-center">
-				Exhibitors
-			</h1>
-			<Swiper
-				modules={[Autoplay]}
-				loop={true}
-				// slidesPerView={6}
-                breakpoints={{
-                    576: {
-                      slidesPerView: 2,
-                    },
-                    768: {
-                      slidesPerView: 4,
-                    },
-                    1024: {
-                        slidesPerView: 6
+type Exhibitor = {
+    sys: {
+        id: string;
+    };
+    name: string;
+    logo: {
+        url: string;
+    };
+};
+
+type ExhibitorsCollection = {
+    items: Exhibitor[];
+};
+
+type Data = {
+    exhibitorsCollection: ExhibitorsCollection;
+};
+
+const Exhibitors: FC<IExhibitors> = () => {
+    const [exhibitors, setExhibitors] = useState<Exhibitor[]>([]);
+
+    useEffect(() => {
+        const getExhibitors = async () => {
+            const data: Data = await graphqlFetcher(`
+            {
+                exhibitorsCollection {
+                    items {
+                        sys {
+                            id
+                        }
+                        name
+                        logo {
+                            url
+                        }
                     }
-                  }}
-				autoplay={{ delay: 2000 }}
-				fadeEffect={{
-					crossFade: true,
-				}}
-                spaceBetween={40}
-			>
-				{data.map(
-					(
-						data: any | null | undefined,
-						index: Key | null | undefined
-					) => (
-						<SwiperSlide key={index}>
-							<div className="flex items-center px-8 sm:px-0 justify-center h-28">
-								<img
-									className=""
-									src={data.image}
-									alt={data.name}
-								/>
-							</div>
-						</SwiperSlide>
-					)
-				)}
-			</Swiper>
-		</div>
-	</SectionWrapper>
-);
+                }
+            }`);
+
+            setExhibitors(data.exhibitorsCollection.items);
+        };
+
+        getExhibitors();
+    }, []);
+
+    return (
+        <SectionWrapper>
+            <div className="py-24">
+                <h1 className="text-2xl sm:text-3xl text-bluePrimary font-semibold mb-4 text-center">
+                    Exhibitors
+                </h1>
+                <Swiper
+                    modules={[Autoplay]}
+                    loop={true}
+                    breakpoints={{
+                        576: {
+                            slidesPerView: 2,
+                        },
+                        768: {
+                            slidesPerView: 4,
+                        },
+                        1024: {
+                            slidesPerView: 6,
+                        },
+                    }}
+                    autoplay={{ delay: 2000 }}
+                    fadeEffect={{
+                        crossFade: true,
+                    }}
+                    spaceBetween={40}
+                >
+                    {exhibitors.map(
+                        (
+                            exhibitor: any | null | undefined,
+                            index: Key | null | undefined
+                        ) => (
+                            <SwiperSlide key={index}>
+                                <div className="flex items-center px-8 sm:px-0 justify-center h-28">
+                                    <img
+                                        className=""
+                                        src={exhibitor.logo.url}
+                                        alt={exhibitor.name}
+                                    />
+                                </div>
+                            </SwiperSlide>
+                        )
+                    )}
+                </Swiper>
+            </div>
+        </SectionWrapper>
+    );
+};
 
 export default Exhibitors;
